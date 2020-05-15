b9cb6cab9231262cd135c116248a804810a3f098
Sanket Patel <3sanket3@gmail.com>
2020-05-15 19:23:43 +0530

    add firebase dev project

--- src/App.js
+++ src/App.js
@@ -20,25 +20,38 @@ import {
   Transactions,
   StockSummary,
 } from "./pages";
-
-firebase.initializeApp({
-  apiKey: "AIzaSyB72n9pfCNfcGsHFcLPLxGIHbOM2j1pcgc",
-  authDomain: "stock-mgmt-3sanket3.firebaseapp.com",
-  databaseURL: "https://stock-mgmt-3sanket3.firebaseio.com",
-  projectId: "stock-mgmt-3sanket3",
-  storageBucket: "stock-mgmt-3sanket3.appspot.com",
-  messagingSenderId: "404925228391",
-  appId: "1:404925228391:web:2cc30d3fed8c3c7f02fbc2",
-  measurementId: "G-LWJWWYEEJL",
-});
-firebase.analytics();
+if (process.env.REACT_APP_BRANCH === "master") {
+  firebase.initializeApp({
+    apiKey: "AIzaSyB72n9pfCNfcGsHFcLPLxGIHbOM2j1pcgc",
+    authDomain: "stock-mgmt-3sanket3.firebaseapp.com",
+    databaseURL: "https://stock-mgmt-3sanket3.firebaseio.com",
+    projectId: "stock-mgmt-3sanket3",
+    storageBucket: "stock-mgmt-3sanket3.appspot.com",
+    messagingSenderId: "404925228391",
+    appId: "1:404925228391:web:2cc30d3fed8c3c7f02fbc2",
+    measurementId: "G-LWJWWYEEJL",
+  });
+  firebase.analytics();
+} else {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCcUA42ZUeAxhg7hh3xIyxKIVvZ9u0y05w",
+    authDomain: "dev-stock-mgmt-3sanket3.firebaseapp.com",
+    databaseURL: "https://dev-stock-mgmt-3sanket3.firebaseio.com",
+    projectId: "dev-stock-mgmt-3sanket3",
+    storageBucket: "dev-stock-mgmt-3sanket3.appspot.com",
+    messagingSenderId: "792191856343",
+    appId: "1:792191856343:web:b39213807de53febb14a92",
+  });
+}
 function App() {
   const [user, setUser] = useState();
 
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       setUser(user);
-      firebase.analytics().setUserId(user.uid);
+      if (process.env.REACT_APP_BRANCH === "master") {
+        firebase.analytics().setUserId(user.uid);
+      }
     } else {
       setUser(null);
     }
